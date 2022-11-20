8d85575e7b4e55339d28c485ce534ddc04e09151
N1P3 <c4xplosivee@gmail.com>
2022-11-20 19:09:53 +0100

    changed algorythm

--- generator.c
+++ generator.c
@@ -6,114 +6,204 @@
 
 bool is_number(const char *num)
 {
-    int idx = strlen(num);
+        int idx = strlen(num);
 
-    while (idx--)
-    {
-        if (num[idx] > 47 && num[idx] < 58)
+        while (idx--)
         {
-            continue;
+                if (num[idx] > 47 && num[idx] < 58)
+                {
+                        continue;
+                }
+                return false;
         }
-        return false;
-    }
-    return true;
-}
-
-void swap(char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void permute(char *a, int l, int r, int len)
-{
-    if (l == r)
-    {
-        for (int i = 0; i < len; i++) {
-            printf("%c ", a[i]);
-        }
-        putchar('\n');
-
-        return;
-    }
-
-    for (int i = l; i <= r; i++)
-    {
-        swap(a + l, a + i);
-        permute(a, l + 1, r, len);
-        swap(a + l, a + i);
-    }
+        return true;
 }
 
 void print_permutations(int arr_num, char *arr_len)
 {
-    if (!strcmp(arr_len, "A"))
-    {
-        char str[arr_num];
-        for (int i = 0; i < arr_num; i++)
+        if (!strcmp(arr_len, "A"))
         {
-            char value = ((i + 1) + '0');
-            str[i] = value;
+                while (arr_num > 9)
+                {
+                        printf("W trosce o twoj dysk prosimy o podanie liczby z zakresu 1-9:\n");
+                        scanf("%d", &arr_num);
+                }
+
+                if (arr_num == 8)
+                {
+                        char set[8] = "12345678";
+                        char scratch;
+                        int lastpermutation = 0;
+                        int i, j, k, l;
+
+                        printf("%s\n", set);
+
+                        while (!lastpermutation)
+                        {
+                                j = -1;
+                                for (i = 0; i < 8; i++)
+                                {
+                                        if (set[i + 1] > set[i])
+                                        {
+                                                j = i;
+                                        }
+                                }
+
+                                if (j == -1)
+                                {
+                                        lastpermutation = 1;
+                                }
+
+                                if (!lastpermutation)
+                                {
+                                        for (i = j + 1; i < 8; i++)
+                                        {
+                                                if (set[i] > set[j])
+                                                {
+                                                        l = i;
+                                                }
+                                        }
+
+                                        scratch = set[j];
+                                        set[j] = set[l];
+                                        set[l] = scratch;
+
+                                        k = (7 - j) / 2;
+
+                                        for (i = 0; i < k; i++)
+                                        {
+                                                scratch = set[j + 1 + i];
+                                                set[j + 1 + i] = set[7 - i];
+                                                set[7 - i] = scratch;
+                                        }
+
+                                        for (int z = 0; z < arr_num; z++)
+                                        {
+                                                printf("%c ", set[z]);
+                                        }
+
+                                        printf("\n");
+                                }
+                        }
+                        return;
+                }
+
+                char set[arr_num];
+
+                for (int p = 0; p < arr_num; p++)
+                {
+                        char value = ((p + 1) + '0');
+                        set[p] = value;
+                        printf("%c ", set[p]);
+                }
+
+                printf("\n");
+
+                char scratch;
+                int lastpermutation = 0;
+                int lownum = (arr_num - 1);
+                int i, j, k, l;
+
+                while (!lastpermutation)
+                {
+                        j = -1;
+                        for (i = 0; i < arr_num; i++)
+                        {
+                                if (set[i + 1] > set[i])
+                                {
+                                        j = i;
+                                }
+                        }
+
+                        if (j == -1)
+                        {
+                                lastpermutation = 1;
+                        }
+
+                        if (!lastpermutation)
+                        {
+                                for (i = j + 1; i < arr_num; i++)
+                                {
+                                        if (set[i] > set[j])
+                                        {
+                                                l = i;
+                                        }
+                                }
+
+                                scratch = set[j];
+                                set[j] = set[l];
+                                set[l] = scratch;
+                                k = ((lownum - j) / 2);
+
+                                for (i = 0; i < k; i++)
+                                {
+                                        scratch = set[j + 1 + i];
+                                        set[j + 1 + i] = set[lownum - i];
+                                        set[lownum - i] = scratch;
+                                }
+
+                                for (int z = 0; z < arr_num; z++)
+                                {
+                                        printf("%c ", set[z]);
+                                }
+
+                                printf("\n");
+                        }
+                }
+                return;
         }
-        printf("\n");
 
-        permute(str, 0, arr_num - 1, arr_num);
-        return;
-    }
-
-    if (!is_number(arr_len))
-    {
-        puts("invalid input");
-        return;
-    }
-
-    srand((unsigned)time(NULL));
-
-    int len = atoi(arr_len);
-    int fin_outp[arr_num];
-    int lower = 1;
-
-    for (int k = 0; k < len; k++)
-    {
-        fin_outp[0] = (rand() % (arr_num + 1 - lower) + lower);
-        printf("%d ", fin_outp[0]);
-
-        for (int i = 1; i < arr_num; i++)
+        if (!is_number(arr_len))
         {
-            fin_outp[i] = (rand() % (arr_num + 1 - lower) + lower);
-
-            int j = 0;
-            while (j < i)
-            {
-                if (fin_outp[i] == fin_outp[j])
-                {
-                    fin_outp[i] = (rand() % (arr_num + 1 - lower) + lower);
-                    j = 0;
-                }
-                else
-                {
-                    j++;
-                }
-            }
-            printf("%d ", fin_outp[i]);
+                puts("invalid input");
+                return;
         }
-        printf("\n");
-    }
+
+        srand((unsigned)time(NULL));
+
+        int len = atoi(arr_len);
+        int fin_outp[arr_num];
+        int lower = 1;
+
+        for (int k = 0; k < len; k++)
+        {
+                fin_outp[0] = (rand() % (arr_num + 1 - lower) + lower);
+                printf("%d ", fin_outp[0]);
+
+                for (int i = 1; i < arr_num; i++)
+                {
+                        fin_outp[i] = (rand() % (arr_num + 1 - lower) + lower);
+
+                        int j = 0;
+                        while (j < i)
+                        {
+                                if (fin_outp[i] == fin_outp[j])
+                                {
+                                        fin_outp[i] = (rand() % (arr_num + 1 - lower) + lower);
+                                        j = 0;
+                                }
+                                else
+                                {
+                                        j++;
+                                }
+                        }
+                        printf("%d ", fin_outp[i]);
+                }
+                printf("\n");
+        }
 }
 
 int main()
 {
-    int number_of_arrs;
-    char length_of_arr[256];
+        int number_of_arrs;
+        char length_of_arr[256];
 
-    puts("Podaj liczbe zbiorow oraz ich dlugosc:");
+        puts("Podaj liczbe zbiorow oraz ich dlugosc:");
 
-    scanf("%d", &number_of_arrs);
-    scanf("%s", length_of_arr);
+        scanf("%d", &number_of_arrs);
+        scanf("%s", length_of_arr);
 
-    print_permutations(number_of_arrs, length_of_arr);
+        print_permutations(number_of_arrs, length_of_arr);
 
-    return 0;
+        return 0;
 }
