bbbfb6046c2c084b4019e16e259e68a30bbb1fe9
Jakub <dask0224@gmail.com>
2022-11-25 16:18:49 +0100

    Updated perm_processing.c and renamed result.out to result.tex
    (result.tex is a more appropriate file name).

--- perm_processing.c
+++ perm_processing.c
@@ -3,29 +3,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "functions_declarations.h"
+
 #define newline fprintf(fptr2, "\n")
 #define tabulator fprintf(fptr2, "\t")
-#define domyslne_parametry FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length
+#define hline fprintf(fptr2, "\t\\hline\n")//Stands for: horizontal line.
 
 //[ENG] Warning! comments are mixed with both polish and english language as of yet.
 //[PL] Uwaga! Póki co język używany w komentarzach jest przeplatany między polskim i angielskim.
 
-//functions described in more detail under the end of the "main" scope.
-long long int silnia(int permutaion_length);
-void/*"char*"?*/ base_permutation2(FILE *fptr2, int permutation_length);
-void permutation_case(domyslne_parametry, int permutation_amount, char flag);
-//funkcje wypisujace do plikow:
-void write_two_line_notation(domyslne_parametry);
-void write_one_line_notation(domyslne_parametry);
-void write_cycle_notation(domyslne_parametry);
-void write_square_of_permutation(domyslne_parametry);
-//"podfunkcja" funkcji write_square_of_permutation:
-void compute_composition(int permutation[], int composed_permutation[], int permutation_length);
-
-char base_permutation[100];
-
-int main() {
-
+int main(int argc, char** argv) {
+    
     
     int permutation[100];
     int permutation_length;
@@ -41,7 +29,7 @@ int main() {
     
     FILE *fptr2;
     
-    fptr2 = fopen("result.out"/*"wygenerowane_permutacje.tex"*/, "a");
+    fptr2 = fopen("result.tex", "a");
     
     switch(permutation_type[0]){
     
@@ -49,7 +37,7 @@ int main() {
             
             permutation_amount = NULL;
             
-            permutation_case(fptr1, fptr2, permutation, permutation_length, permutation_amount, 0);
+            permutation_case(fptr1, fptr2, permutation, permutation_length, permutation_amount, 0, argv);
             
             break;
         
@@ -57,7 +45,7 @@ int main() {
         
             permutation_amount = atoi(permutation_type);           
             
-            permutation_case(fptr1, fptr2, permutation, permutation_length, permutation_amount, 1);
+            permutation_case(fptr1, fptr2, permutation, permutation_length, permutation_amount, 1, argv);
             
             break;
         
@@ -79,124 +67,124 @@ long long int silnia(int permutaion_length){
 
 }
 
-void base_permutation2(FILE *fptr2, int permutation_length){//writes base permutation i. e: prints first "row" of the two-line notation's permutation to char base_permutation[].
+void base_permutation(FILE *fptr2, int permutation_length){//writes base permutation i. e: prints first "row" of the two-line notation's permutation to char base_permutation[].
     
     for(int i = 0; i < permutation_length; i++) {fprintf(fptr2, "%d ", i+1); if(i!=permutation_length-1) fprintf(fptr2, "& ");}
 
 }
 
 //wypisuje output do pliku, w zaleznosci od tego co podał użytkownik na wejściu (case'a, który został wybrany).
-void permutation_case(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length, int permutation_amount, char perm_case){
+void permutation_case(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length, int permutation_amount, char perm_case, char** argv){
 
     
         long long int zakres_petli;
         
-        fprintf(fptr2, "\\subsection*{Wygenerowana permutacja nr 1:}\n");
+        fprintf(fptr2, "\\section{Wygenerowany rodzaj permutacji nr %s:}\n", argv[1]);
         
         if(perm_case == 0){
-            fprintf(fptr2, "\\subsection*{Uzytkownik zazadal wypisania wszystkich mozliwych\\\\ permutacji %d-elementowych.}\n", permutation_length);
+            fprintf(fptr2, "\\subsection*{Użytkownik zażądał wypisania wszystkich możliwych\\\\ permutacji %d-elementowych.}\n\n", permutation_length);
             zakres_petli = silnia(permutation_length);
         }else/*(dla perm_case równego 1)*/{
-            fprintf(fptr2, "\\subsection*{Uzytkownik zazadal wypisania %d losowych permutacji %d-elementowych.}\n", permutation_amount, permutation_length);
+            fprintf(fptr2, "\\subsection*{Użytkownik zażądał wypisania %d losowych permutacji %d-elementowych.}\n\n", permutation_amount, permutation_length);
             zakres_petli = permutation_amount;
         }
         
         for(long long int j = 0; j < zakres_petli; j++){
 
             for(int i = 0; i < permutation_length; i++) fscanf(fptr1, "%d", permutation+i);
-
-            //two-line notation.
-            //zapis permutacji w dwoch liniach (two-line notation):;
+            
+            
+            fprintf(fptr2, "\\subsection{}\n");
+            
+            fprintf(fptr2, "\\begin{center}\n");
+            
+            write_current_permutation(fptr1, fptr2, permutation, permutation_length);
+            
+            fprintf(fptr2, "\\begin{tabular}{|m{0.6\\linewidth}|m{0.4\\linewidth}|}\n");
+            hline;
+            
+            fprintf(fptr2, "\tNazwa Parametru & Parametr \\\\\n");
+            hline;
             
             write_two_line_notation(fptr1, fptr2, permutation, permutation_length);
 
-            //one-line notation
-            //zapis permutacji w jednej linii (one-line notation):
             write_one_line_notation(fptr1, fptr2, permutation, permutation_length);
-
-            //cycle notation permutation
-            //printf("zapis permutacji w postaci cyklicznej (cycle notation): \n");
 
             write_cycle_notation(fptr1, fptr2, permutation, permutation_length);
 
-            //złożenie permutacji samej ze sobą (permutation's square?)
-            //printf("zlozenie permutacji samej ze soba: \n");
-
             write_square_of_permutation(fptr1, fptr2, permutation, permutation_length);
+            
+            fprintf(fptr2, "\\end{tabular}\n");
+            
+            fprintf(fptr2, "\\end{center}\n");
 
-            newline;
+            newline; newline;
 
         }
         
 }
 
-void write_two_line_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length){
-    
-    fprintf(fptr2, "\\subsection*{two line notation:}\n");
-    fprintf(fptr2, "\\[\n");
+void write_current_permutation(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length){
 
+    fprintf(fptr2, "\\[\n");
+    
     fprintf(fptr2, "\\begin{pmatrix}\n");
     tabulator;
-    base_permutation2(fptr2, permutation_length);
+    base_permutation(fptr2, permutation_length);
     fprintf(fptr2, "\\\\ \n");
     tabulator;
     for(int i = 0; i < permutation_length; i++) {fprintf(fptr2, "%d ", permutation[i]); if(i!=permutation_length-1) fprintf(fptr2, "& ");}
     newline;
     fprintf(fptr2, "\\end{pmatrix}\n");
-
+    
     fprintf(fptr2, "\\]\n");
     newline;
+    
+}
+
+void write_two_line_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length){
+    
+    fprintf(fptr2, "\tzapis permutacji w dwoch liniach (two-line notation) & $\\begin{pmatrix} ");
+    base_permutation(fptr2, permutation_length);
+    fprintf(fptr2, "\\\\ \n");
+    for(int i = 0; i < permutation_length; i++) {fprintf(fptr2, "%d ", permutation[i]); if(i!=permutation_length-1) fprintf(fptr2, "& ");}
+    fprintf(fptr2, "\\end{pmatrix}$ \\\\ \n");
+
+    hline;
     
 }
 
 void write_one_line_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length){
     
-    fprintf(fptr2, "\\subsection*{one line notation:}\n");
-    fprintf(fptr2, "\\[\n");
-
-    fprintf(fptr2, "\\begin{pmatrix}\n");
-    tabulator;
+    fprintf(fptr2, "\tzapis permutacji w jednej linii (one-line notation) & $\\begin{pmatrix} ");
     for(int i = 0; i < permutation_length; i++) {fprintf(fptr2, "%d ", permutation[i]); if(i!=permutation_length-1) fprintf(fptr2, "& ");}
-    newline;
-    fprintf(fptr2, "\\end{pmatrix}\n");
+    fprintf(fptr2, "\\end{pmatrix}$ \\\\ \n");
 
-    fprintf(fptr2, "\\]\n");
-    newline;
+    hline;
     
 }
 
 void write_square_of_permutation(FILE *fptr1, FILE *fptr2, int permutation[], int permutation_length){
 
-    fprintf(fptr2, "\\subsection*{square of permutation:}\n");
-    fprintf(fptr2, "\\[\n");
-
-    fprintf(fptr2, "\\begin{pmatrix}\n");
-    tabulator;
-    base_permutation2(fptr2, permutation_length);
+    fprintf(fptr2, "\tzlozenie permutacji samej ze soba (kwadrat permutacji (permutation's square)) & $\\begin{pmatrix} ");
+    base_permutation(fptr2, permutation_length);
     fprintf(fptr2, "\\\\ \n");
-    tabulator;
-    int composed_permutation[permutation_length];
-    compute_composition(permutation, composed_permutation, permutation_length);
+    int composed_permutation[permutation_length];//--
+    compute_composition(permutation, composed_permutation, permutation_length);//--
     for(int i = 0; i < permutation_length; i++) {fprintf(fptr2, "%d ", composed_permutation[i]); if(i!=permutation_length-1) fprintf(fptr2, "& ");}
-    newline;
-    fprintf(fptr2, "\\end{pmatrix}\n");
+    fprintf(fptr2, "\\end{pmatrix}$ \\\\ \n");
 
-    fprintf(fptr2, "\\]\n");
-    newline;
+    hline;
 
 }
 
 void compute_composition(int permutation[], int* composed_permutation, int permutation_length){//calculates the square of particular permutation.
-
-    //int* ptr = composed_permutation;
     
     for(int i = 0; i < permutation_length; i++){
       //to samo co: composed_permutation+i (?)
         composed_permutation[i] = permutation[permutation[i]-1];
 
     }
-    
-    //return ptr;
 
 }
 
@@ -206,8 +194,7 @@ void write_cycle_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permu
     bool used_indexes[9] = {0};//scisle zwiazane z permutation_length.
     int next_index;//nastepny indeks, ktory ma byc sprawdzany
     
-    fprintf(fptr2, "\\subsection*{cycle notation permutation:}\n");
-    fprintf(fptr2, "\\[\n");
+    fprintf(fptr2, "\tzapis permutacji w postaci cyklicznej (cycle notation) & $");
 
     for(int i = 0; i < permutation_length; i++){//najpierw sprawdzany czy istnieje cykl liczby samej ze soba.
 
@@ -216,7 +203,7 @@ void write_cycle_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permu
             used_indexes[i] = 1;
             fprintf(fptr2, "\\begin{pmatrix} ");
             fprintf(fptr2, "%d ", permutation[i]);
-            fprintf(fptr2, "\\end{pmatrix}\n");
+            fprintf(fptr2, "\\end{pmatrix} ");
             
         }
 
@@ -238,7 +225,7 @@ void write_cycle_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permu
                 if(permutation[next_index] == i+1){
 
                     fprintf(fptr2, "%d ", next_index+1);
-                    fprintf(fptr2, "\\end{pmatrix}\n");
+                    fprintf(fptr2, "\\end{pmatrix} ");
                     used_indexes[next_index] = 1;
                     finished = 1;
 
@@ -256,9 +243,9 @@ void write_cycle_notation(FILE *fptr1, FILE *fptr2, int permutation[], int permu
         }
 
     }
-
     
-    fprintf(fptr2, "\\]\n");
-    newline;
+    fprintf(fptr2, "$ \\\\ \n");
+    
+    hline;
 
 }
