008e4c1c16c6c187719d2b8b3790ea715b56380c
Jakub <dask0224@gmail.com>
2022-12-07 17:51:15 +0100

    Updated the perm.sh + added the list of error codes.

--- perm.sh
+++ perm.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
+bug_check(){
+#gdzie: $1 to komunikat wypisywany dla uzytkownika, a $2 to error exit code.
+	if (($? != 0))
+	then 
+		echo $1
+		exit $2
+	fi
+
+}
+
 cd CBuild/
+
+touch make.log
+bug_check 'Nie udalo sie utworzyc pliku "make.log".' 10
+
+chmod 766 make.log
+bug_check 'Nie udalo sie zmienic uprawnien dla pliku "make.log".' 11
+
 make 2> make.log
 cd ..
 
 x=$(head -1 user_input.txt) #pobiera liczbę permutacji do wygenerowania, podaną przez użytkownika.
 
 rm -f result.tex
+bug_check 'Nie udalo sie usunac pliku "result.tex" z poprzedniego wykonania programu.' 12
 
+touch result.tex
+bug_check 'Nie udalo sie utworzyc pliku "result.tex". Koncze dzialanie programu.' 13
+
+chmod u+rwx result.tex
+bug_check 'Nie udalo sie zmienic uprawnien dla pliku "result.tex". Koncze dzialanie programu.' 14
 
 if (($(wc -l user_input.txt | cut -d" " -f1)-1 > $x))
 then
@@ -35,6 +58,12 @@ echo "Zażądana ilość osobnych rodzajów permutacji do wygenerowania: $x" >> 
 echo '\end{flushleft}' >> result.tex
 echo '\hfill' >> result.tex
 
+touch gen.log
+bug_check 'Nie udalo sie utworzyc pliku "gen.log". Koncze dzialanie programu.' 15
+
+chmod u+rw gen.log
+bug_check 'Nie udalo sie zmienic uprawnien dla pliku "gen.log". Koncze dzialanie programu.' 16
+
 var1=1
 while (($var1 <= $x))
 do
@@ -46,5 +75,20 @@ done
 echo '\end{document}' >> result.tex
 
 rm -f result.pdf
+bug_check 'Nie udalo sie usunac pliku "result.tex" z poprzedniego wykonania programu.' 17
+
+touch result.pdf
+bug_check 'Nie udalo sie usunac pliku "result.tex" z poprzedniego wykonania programu.' 18
+
+chmod u+rwx result.pdf
+bug_check 'Nie udalo sie zmienic uprawnien dla pliku "result.tex". Koncze dzialanie programu.' 19
+
+touch tex.log
+bug_check 'Nie udalo sie utworzyc pliku "gen.log". Koncze dzialanie programu.' 20
+
+chmod u+rw tex.log
+bug_check 'Nie udalo sie zmienic uprawnien dla pliku "gen.log". Koncze dzialanie programu.' 21
 
 pdflatex result.tex > tex.log
+
+bug_check 'Blad programu pdflatex.' 30
