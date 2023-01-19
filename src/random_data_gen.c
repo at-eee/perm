20045408b2ceff3d81ff1fc693509586594c9761
Paweł Rybak <pawel.rybak13@gmail.com>
2023-01-19 13:29:44 +0100

    Finished random data generator script

--- src/random_data_gen.c
+++ src/random_data_gen.c
@@ -4,12 +4,14 @@
 #include <string.h>
 #include <errno.h>
 
-static const char len_characters[] = { 'A', 'B', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+static const char len_characters[] = { 'A', 'B', 'C', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-// ./rdg [samples count] [lower bound] [upper bound]
+// ./rdg [samples count] [upper bound]
 
-void generate_random_pair(FILE *file) {
-    puts("yo");
+void generate_random_pair(FILE *file, int upper) {
+    int upper_bound = (rand() % upper) + 1;
+    int rand_idx = rand() % (sizeof(len_characters) / sizeof(char));
+    fprintf(file, "%d %c\n", upper_bound, len_characters[rand_idx]);
 }
 
 int main(int argc, char **argv) {
@@ -22,11 +24,12 @@ int main(int argc, char **argv) {
 
     // TODO (wedkarz): add asserts for input
     int samples_count = atoi(argv[1]);
+    int upper_bound = atoi(argv[2]);
     
     FILE *f = fopen("../user_input.txt", "w");
     fprintf(f, "%d\n", samples_count);
     for (int i = 0; i < samples_count; i++) {
-        generate_random_pair(f);
+        generate_random_pair(f, upper_bound);
     }
     fclose(f);
 
