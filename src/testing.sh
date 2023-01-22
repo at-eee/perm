6b2248b320679a7900c98c4803ffeb977cb86ca8
Jakub <dask0224@gmail.com>
2023-01-22 17:49:14 +0100

    testing.sh - Added some functionality and put some of the instructions into functions (for better code readability)

--- src/testing.sh
+++ src/testing.sh
@@ -8,6 +8,41 @@ bug_check(){
 		echo 'Przerywam dzialanie programu.'
 		exit $2
 	fi
+}
+
+#rysuje i zapisuje wykresu do chart$chart_count.png
+rysuj_wykres(){
+
+	cd test/
+	gnuplot instructions_for_gnuplot.p
+	mv chart.png chart$chart_count.png
+	((chart_count++))
+	cd ..
+}
+
+#wstawia wykres do pliku test/stat.tex
+wstaw_wykres(){
+
+#gdzie $1 to parametr przekazywany funkcji i mowiacy o tym z jakim typem testu mamy do czynienia.
+
+	echo '\begin{figure}[h]' >> test/stat.tex
+	echo '\centering' >> test/stat.tex
+	echo "\\includegraphics[width=0.60\\textwidth]{chart$(($chart_count-1)).png}" >> test/stat.tex
+	echo "\\caption{wykres dla podtestow, testu typu $1.}" >> test/stat.tex
+	echo '\end{figure}' >> test/stat.tex
+
+}
+
+#Procedura rozpoczynania tabeli jest taka sama, dla kazdego rodzaju testu.
+#Dlatego, w celu "odchudzenia" i zrobienia kodu bardziej czytelnym umieszczono te instrukcje w jednej funkcji.
+rozpocznij_rysowanie_tabeli(){
+
+	echo '\begin{table}[h]' >> test/stat.tex
+	echo '\centering' >> test/stat.tex
+	echo '\begin{tabularx}{0.65\textwidth}{|X|X|}' >> test/stat.tex
+	echo '\hline' >> test/stat.tex
+	echo 'wielkosc podtestu & czas wykonywania (s) \\' >> test/stat.tex
+	echo '\hline' >> test/stat.tex
 
 }
 
@@ -70,14 +105,12 @@ while (($i < $amount))
 do
 	if [ $(head -$line ./test/parameters.txt | tail -1 | cut -d ' ' -f1) = 'A' ]
 	then
-		((line++))
 		((i++))
-		#echo 'Test typu A' >> test/stat.tex
-		echo '\begin{table}[h]' >> test/stat.tex
-		echo '\centering' >> test/stat.tex
-		echo '\begin{tabularx}{0.60\textwidth}{|X|X|}' >> test/stat.tex
-		echo '\hline' >> test/stat.tex
-		echo '' > test/for_chart.txt
+		((line++))
+		echo '\section{Test typu A}' >> test/stat.tex
+		#rozpoczyna tabele w pliku test/stat.tex
+		rozpocznij_rysowanie_tabeli
+		echo '' > test/for_chart.txt #for_chart.txt!
 		in1=$(head -$(($line)) ./test/parameters.txt | tail -1) #in1 to skrot od input (in.)
 		bug_check 'Blad odczytu z pliku parameters.txt' 49
 		#powysza linia wczytuje caly tekst (wszystkie liczby) z danej linii w "parameters.txt" do zmiennej in1 w teorii bedaca tablica liczb (w rzeczywistosci bedacej po prostu tekstem (jak wiele rzeczy w bashu)).
@@ -90,13 +123,11 @@ do
 			touch time.txt
 			bug_check 'Nie udalo sie utworzyc pliku "time.txt"' 53
 
-			#czas=$(time ./perm.sh | tail -3 | head -1 | cut -d' ' -f2)
 			(time ./perm.sh) 2> time.txt
 			czas=$(tail -3 time.txt | head -1 | cut -f2)
 			echo $czas > time.txt
 
 			milisec=$(grep -o "[0-9][0-9][0-9]" time.txt)
-			#alternatywnie: $(grep -E -o "[0-9]+" time.txt | tail -1)
 			sec=$(grep -E -o "[0-9]+" time.txt | tail -2 | head -1)
 			min=$(grep -E -o "[0-9]+" time.txt | head -1)
 			((sec=sec+min*60))
@@ -104,56 +135,67 @@ do
 
 			echo "$var2 & $sec.$milisec \\\\" >> test/stat.tex
 			echo '\hline' >> test/stat.tex
-			
-			#echo $czas > time.txt
-
-			#milisec=$(grep -o "[0-9][0-9][0-9]" time.txt)
-			##alternatywnie: $(grep -E -o "[0-9]+" time.txt | tail -1)
-			#sec=$(grep -E -o "[0-9]+" time.txt | tail -2 | head -1)
-			#min=$(grep -E -o "[0-9]+" time.txt | head -1)
-			#((sec=sec+min*60))
-			#echo "$var2 $sec.$milisec" >> test/for_chart.txt
 
 			echo $i
 			((i++)) #ta operacja dzieje sie dodatkowo (by orientowac sie przy ktorym parametrze w pliku parameters.txt jestesmy), poniewaz i tak odwolujmy sie do perm za pomoca liczby var2 (z liczb w in1).
 		done
 		((line++))
+		#zakancza dana tabele
 		echo '\end{tabularx}' >> test/stat.tex
 		echo '\end{table}' >> test/stat.tex
-		cd test/
-		gnuplot instructions_for_gnuplot.p
-		mv chart.png chart$chart_count.png
-		((chart_count++))
-		cd ..
-		echo '\begin{figure}[h]' >> test/stat.tex
-		echo '\centering' >> test/stat.tex
-		echo "\\includegraphics[width=0.60\\textwidth]{chart$(($chart_count-1)).png}" >> test/stat.tex
-		echo '\caption{wykres dla podtestow, testu typu A.}' >> test/stat.tex
-		echo '\end{figure}' >> test/stat.tex
+		#rysowanie i zapisywanie wykresu do chart$chart_count.png
+		rysuj_wykres
+		#umieszczanie wykresu w pliku test/stat.tex
+		wstaw_wykres 'A'
+
 	elif [ $(head -$line ./test/parameters.txt | tail -1 | cut -d ' ' -f 1) = 'B' ]
 	then
-		#echo 'Test typu B' >> test/stat.tex
-		echo '\begin{table}[h]' >> test/stat.tex
-		echo '\centering' >> test/stat.tex
-		echo '\begin{tabularx}{0.60\textwidth}{|X|X|}' >> test/stat.tex
-		echo '\hline' >> test/stat.tex
+		echo '\section{Test typu B}' >> test/stat.tex
+		#rozpoczyna tabele w pliku test/stat.tex
+		rozpocznij_rysowanie_tabeli
+		echo '' > test/for_chart.txt #for_chart.txt!
 		dlugosc_permutacji=$(head -$(($line)) ./test/parameters.txt | tail -1 | cut -d ' ' -f2)
+		bug_check 'Blad odczytu z pliku parameters.txt' 49
+		((i+=2))
 		((line++))
 		in1=$(head -$(($line)) ./test/parameters.txt | tail -1) #in1 to skrot od input (in.)
 		#powysza linia wczytuje caly tekst (wszystkie liczby) z danej linijki "parameters.txt" do zmiennej in1 w teorii bedaca tablica liczb (w rzeczywistosci bedacej po prostu tekstem (jak wiele rzeczy w bashu)).
 		for var3 in $in1
 		do
-			echo 'B'
+			echo "1" > user_input.txt
+			bug_check 'Nie udalo sie nadpisac pliku user_input.txt' 47
+			echo "$dlugosc_permutacji $var3" >> user_input.txt
+			bug_check 'Nie udalo sie nadpisac pliku user_input.txt' 47
+			touch time.txt
+			bug_check 'Nie udalo sie utworzyc pliku "time.txt"' 53
+
+			(time ./perm.sh) 2> time.txt
+			czas=$(tail -3 time.txt | head -1 | cut -f2)
+			echo $czas > time.txt
+
+			milisec=$(grep -o "[0-9][0-9][0-9]" time.txt)
+			sec=$(grep -E -o "[0-9]+" time.txt | tail -2 | head -1)
+			min=$(grep -E -o "[0-9]+" time.txt | head -1)
+			((sec=sec+min*60))
+			echo "$var3 $sec.$milisec" >> test/for_chart.txt
+
+			echo "$var3 & $sec.$milisec \\\\" >> test/stat.tex
+			echo '\hline' >> test/stat.tex
+
 			echo $i
-			#cos tam, rob rdg, wrzuc do perm cos tam.
 			((i++)) #ta operacja dzieje sie dodatkowo (by orientowac sie przy ktorym parametrze w pliku parameters.txt jestesmy), poniewaz i tak odwolujmy sie do perm za pomoca var3.
 		done
 		((line++))
+		#zakancza dana tabele
 		echo '\end{tabularx}' >> test/stat.tex
 		echo '\end{table}' >> test/stat.tex
+		#rysowanie i zapisywanie wykresu do chart$chart_count.png
+		rysuj_wykres
+		#umieszczanie wykresu w pliku test/stat.tex
+		wstaw_wykres 'B'
+
 	fi
 	
-	((i++))
 done
 
 echo '' >> test/stat.tex 
